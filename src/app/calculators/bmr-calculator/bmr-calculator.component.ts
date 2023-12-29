c835f2838cd48a37793d8cab6680d5270abd0f03
Diego <diegonunesftw@gmail.com>
2023-12-29 00:23:24 -0300

    feat: home component

---
+++ src/app/calculators/bmr-calculator/bmr-calculator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bmr-calculator',
+  templateUrl: './bmr-calculator.component.html',
+  styleUrls: ['./bmr-calculator.component.css']
+})
+export class BmrCalculatorComponent {
+
+}
